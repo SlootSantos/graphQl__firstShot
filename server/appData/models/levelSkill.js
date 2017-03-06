3798631d761446f187ba678591fe4a43fde0107e
Adam Żaczek <adam.zaczek@netguru.pl>
2017-03-06 22:46:19 +0100

    Update Seeder And One Model

--- server/appData/models/levelSkill.js
+++ server/appData/models/levelSkill.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const LEVEL_SKILL = mongoose.model('Level_Skill', new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   level: { type: Number },
-  favorite: { type: Boolean }
+  favorite: { type: Boolean },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  skill: { type: mongoose.Schema.Types.ObjectId, ref: 'Skill' }
 }));
 
 export default LEVEL_SKILL;
